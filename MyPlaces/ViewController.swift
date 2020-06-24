f9d9d9fc80031a0e206423cf4effc0117a4c5ae2
Ivan Belyakov <ibelyakov@me.com>
2020-06-24 18:45:09 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Ivan Belyakov on 24.06.2020.
+//  Copyright © 2020 Ivan Belyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
