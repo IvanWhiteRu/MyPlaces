cc9a0ef13d534f2eb9302746d2c203e77c83a190
Ivan Belyakov <ibelyakov@me.com>
2020-06-24 22:16:46 +0300

    App Icon Launch screen cell height

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -36,9 +36,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius  = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
     
+    //MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
     
     //MARK: - Navigation
 
