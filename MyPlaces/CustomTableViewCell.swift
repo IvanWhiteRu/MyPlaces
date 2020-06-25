785da6404a6311fd86ede6b903136bf8bd662224
Ivan Belyakov <ibelyakov@me.com>
2020-06-25 23:26:59 +0300

    Custom Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Ivan Belyakov on 24.06.2020.
+//  Copyright © 2020 Ivan Belyakov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+
+    
+    
+    
+}
